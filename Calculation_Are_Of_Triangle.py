ebcfe8d2d29115014ce0576e90a2b9d3601b2e1f
Berke Şen <113352134+TechWiseCompany@users.noreply.github.com>
2023-07-08 01:21:05 +0300

    Create Calculation_Are_Of_Triangle.py

---
+++ Calculation_Are_Of_Triangle.py
@@ -0,0 +1,10 @@
+"""
+If you have to calculate area of triangle you need to know two information about triangle.
+1-)Height(h)
+2-)the base(a)
+"""
+
+h = int(input("Input the height==> "))
+a = int(input("Input the base==> "))
+area = a*h/2
+print(area)
