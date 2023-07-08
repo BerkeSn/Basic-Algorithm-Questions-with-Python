084db936489aaf250429cb7aef87bc02f85d018c
Berke Şen <113352134+TechWiseCompany@users.noreply.github.com>
2023-07-08 17:35:49 +0300

    Create divisors_of_an_integer.py

---
+++ divisors_of_an_integer.py
@@ -0,0 +1,12 @@
+"""
+If you need to calculate divisors of an integer numbers you can use my code.
+"""
+
+
+
+A = int(input("Input an integer number==> "))
+list=[]
+for i in range(1,A+1):
+    if A%i==0:
+        list.append(i)
+print(A," list of divisors of A:", list)
